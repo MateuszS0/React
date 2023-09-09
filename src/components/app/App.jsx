cb961280ef712905a0ef739f5152587a26922371
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2023-09-09 18:18:42 +0200

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,6 +6,7 @@ import Skills from '../skills/Skills';
 import Projects from '../projects/Projects';
 //overflow-x on skills for mobile
 //aspect-ratio 16/9 on images
+//learn scrolling from component to component
 function App() {
   return (
     <>
