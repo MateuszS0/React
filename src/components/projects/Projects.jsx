5abbf6d236e5c5114746796b09202272d6071040
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2023-09-09 18:26:53 +0200

    Project-component subText

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,11 +11,21 @@ const Projects = () => {
         <div className="container-p">
         <div className="project-container">
         <a href="https://github.com/MateuszS0/React-Portfolio" className='a'><img src={Picture} alt="" className='pictures'/></a>
+        <h2>React Portfolio</h2>
+        <h5>My Portfolio</h5>
+        </div>
+        <div className="project-container">
+        <img src="" alt="" />
+        <a href="https://github.com/MateuszS0/MyCode/tree/main/Wwwebflix"className='a'><img src={wwwebflix} alt="" className='pictures'/></a>
+        <h2>Webflix</h2>
+        <h5>Websites catalogue</h5>
         </div>
 
         <div className="project-container">
         <img src="" alt="" />
         <a href="https://github.com/MateuszS0/MyCode/tree/main/Wwwebflix"className='a'><img src={wwwebflix} alt="" className='pictures'/></a>
+        <h2>Timed Math Challenge</h2>
+        <h5>Python Project</h5>
         </div>
         </div>
 
