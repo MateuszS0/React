3f927d3b030eedacfcdc2ded17ce2e6faa31364d
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2023-11-06 14:07:41 +0100

    style: text center

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -14,26 +14,26 @@ const Projects = () => {
         <div className="project-container">
         <a href="https://github.com/MateuszS0/React-Portfolio" className='a'><img src={Picture} alt="Portfolio" className='pictures'/></a>
         <h2>React Portfolio</h2>
-        <h5>My Portfolio</h5>
+        <h5 className='project-desc'>My Portfolio</h5>
         </div>
         <div className="project-container">
         <img src="" alt="" />
         <a href="https://github.com/MateuszS0/MyCode/tree/main/Wwwebflix"className='a'><img src={wwwebflix} alt="JavaScript" className='pictures'/></a>
         <h2>Webflix</h2>
-        <h5>Websites catalogue</h5>
+        <h5 className='project-desc'>Websites catalogue</h5>
         </div>
 
         <div className="project-container">
         <img src="" alt="" />
         <a href="https://github.com/MateuszS0/Python/tree/main/Projects/Timed%20Math%20Challenge"className='a'><img src={pythonLogo} alt="Python" className='pictures'/></a>
         <h2>Timed Math Challenge</h2>
-        <h5>Python Project</h5>
+        <h5 className='project-desc'>Python Project</h5>
         </div>
         <div className="project-container">
         <img src="" alt="" />
         <a href="https://github.com/MateuszS0/PriceAlert"className='a'><img src={TypeScript} alt="TypeScript" className='pictures'/></a>
         <h2>PriceAlert</h2>
-        <h5>Website for tracking prices for amazon products</h5>
+        <h5 className='project-desc'>Website for tracking prices for amazon products</h5>
         </div>
         <div className="project-container">
         <img src="" alt="" />
