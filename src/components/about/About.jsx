46ca033c4e767e8166c0ae8dba05060a4442cf53
Mateusz Stolarczyk <mateusz.stolarczyk2@gmail.com>
2023-11-23 15:24:01 +0100

    chore: text update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <h2>About Me</h2>
       <div className='wrapper'>
       <img src={Photo} className='photo' />
-      <div className="about_me">Hello! My name is Mateusz Stolarczyk, I'm 21 years old and my hobby is programming, i love learning new things and I'm not afraid of challenges. I've been programming since i was 15 years old, back then i was making simple html websites with only a little amount of css code for fun but now I'm trying to make more difficult websites with the usage of multiple technologies. This is a simple portfolio i made in a few days and I am still updating it. I hope you like it! </div>
+      <div className="about_me">Hello! My name is Mateusz Stolarczyk, I'm 22 years old and my hobby is programming, I love learning new things and I'm not afraid of challenges. I've been programming since i was 15 years old, back then i was making simple html websites with only a little amount of css code for fun but now I'm trying to make more difficult websites with the usage of multiple technologies. This is a simple portfolio i made in a few days and I am still updating the projects section. I hope you like it! </div>
       </div>
 
     </div>
